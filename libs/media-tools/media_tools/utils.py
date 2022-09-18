1134c4941798d69faf84fe445899a0dd985cbd91
Luiz Felipe Manke <luizmanke@gmail.com>
2022-09-18 20:15:14 -0300

    add linters

---
+++ libs/media-tools/media_tools/utils.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+import os
+
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+class Credentials(SpotifyClientCredentials):
+    pass
+
+
+class Provider(Spotify):
+
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        super().__init__(client_credentials_manager=Credentials(client_id, client_secret))
+
+
+@lru_cache
+def get_provider() -> Provider:
+    return Provider(
+        os.environ.get("CLIENT_ID", ""),
+        os.environ.get("CLIENT_SECRET", "")
+    )
