e6e0264be59dc4b0df7a7b935a7c4fc22aa41a6f
Luiz Felipe Manke <luizmanke@gmail.com>
2022-10-25 22:48:48 -0300

    Add all in all out security group

--- infra/databases/medias/__main__.py
+++ infra/databases/medias/__main__.py
@@ -2,14 +2,41 @@ import pulumi
 import pulumi_aws as aws
 
 
-rds = aws.rds.Instance(
-    "rds-instance",
+sg_all_in_all_out = aws.ec2.SecurityGroup(
+    "all-in-all-out",
+    description="Allow all inbound and outbound traffic",
+    name="all-in-all-out",
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=0,
+            to_port=0,
+            protocol="-1",
+            cidr_blocks=["0.0.0.0/0"],
+        )
+    ],
+    egress=[
+        aws.ec2.SecurityGroupEgressArgs(
+            from_port=0,
+            to_port=0,
+            protocol="-1",
+            cidr_blocks=["0.0.0.0/0"],
+        )
+    ]
+)
+
+rds_medias = aws.rds.Instance(
+    "medias",
     allocated_storage=50,
+    backup_retention_period=7,
     engine="postgres",
     engine_version="14.2",
+    identifier="medias-qa",
     instance_class="db.t3.micro",
     db_name="medias",
+    port=5432,
     username="username123456",
     password="password123456",
+    publicly_accessible=True,
     skip_final_snapshot=True,
+    vpc_security_group_ids=[sg_all_in_all_out.id]
 )
