46ac77086db0c3376985a0890dab66f77ba6eb1d
Luiz Felipe Manke <luizmanke@gmail.com>
2022-11-05 19:20:39 -0300

    Minor changes

--- pipelines/tests/unit/jobs/test_batch_database_update.py
+++
@@ -1,9 +0,0 @@
-from pipelines.jobs.batch_database_update import batch_database_update
-
-
-def test_pipeline():
-
-    result = batch_database_update.execute_in_process()
-
-    assert result.success
-    assert result.output_for_node("get_number") == 200
