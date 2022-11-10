ecb4c0894e2086eb9e243420bb0d0480ef0a78c1
Luiz Felipe Manke <37818079+luizmanke@users.noreply.github.com>
2022-11-09 22:02:36 -0300

    refactor: Update media tools provider (#32)

--- libs/media-tools/media_tools/search.py
+++ libs/media-tools/media_tools/search.py
@@ -1,35 +1,44 @@
 from typing import List
 
 from loguru import logger
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
 
 from media_tools import schemas
-from media_tools import utils
 
 
-def get_artists(query: str) -> List[schemas.Artist]:
+class Credentials(SpotifyClientCredentials):
+    pass
 
-    MAX_ITEMS_PER_REQUEST = 50
-    MAX_OFFSET = 1_000
-    provider = utils.get_provider()
 
-    offset = 0
-    artists = []
-    for offset in range(0, MAX_OFFSET, MAX_ITEMS_PER_REQUEST):
-        logger.debug(f"Requesting offset {offset}.")
+class Provider(Spotify):
 
-        response = provider.search(query, type="artist", limit=MAX_ITEMS_PER_REQUEST, offset=offset)
-        for item in response["artists"]["items"]:
-            artists.append(
-                schemas.Artist(
-                    id=item["id"],
-                    name=item["name"],
-                    n_followers=item["followers"]["total"],
-                    genres=item["genres"],
-                    popularity=item["popularity"]
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        super().__init__(client_credentials_manager=Credentials(client_id, client_secret))
+
+    def get_artists(self, query: str) -> List[schemas.Artist]:
+
+        MAX_ITEMS_PER_REQUEST = 50
+        MAX_OFFSET = 1_000
+
+        offset = 0
+        artists = []
+        for offset in range(0, MAX_OFFSET, MAX_ITEMS_PER_REQUEST):
+            logger.debug(f"Requesting offset {offset}.")
+
+            response = self.search(query, type="artist", limit=MAX_ITEMS_PER_REQUEST, offset=offset)
+            for item in response["artists"]["items"]:
+                artists.append(
+                    schemas.Artist(
+                        id=item["id"],
+                        name=item["name"],
+                        n_followers=item["followers"]["total"],
+                        genres=item["genres"],
+                        popularity=item["popularity"]
+                    )
                 )
-            )
 
-        if len(artists) >= response["artists"]["total"]:
-            break
+            if len(artists) >= response["artists"]["total"]:
+                break
 
-    return artists
+        return artists
--- libs/media-tools/tests/unit/test_search.py
+++ libs/media-tools/tests/unit/test_search.py
@@ -1,9 +1,11 @@
+from typing import Dict
+
 import pytest
 
-from media_tools import search
+from media_tools.search import Provider
 
 
-class MockProvider:
+def fake_search(total_samples: int) -> Dict:
 
     MAX_SAMPLES_PER_SEARCH = 50
     SAMPLE = {
@@ -14,53 +16,56 @@ class MockProvider:
         "popularity": 100
     }
 
-    def __init__(self, total_samples):
-        self.total_samples = total_samples
-
-    def search(self, *args, **kwargs):
-        n_samples = min(self.total_samples, self.MAX_SAMPLES_PER_SEARCH)
-        return {
-            "artists": {
-                "items": [self.SAMPLE] * n_samples,
-                "total": self.total_samples
-            }
+    return {
+        "artists": {
+            "items": [SAMPLE] * min(total_samples, MAX_SAMPLES_PER_SEARCH),
+            "total": total_samples
         }
+    }
 
 
 @pytest.fixture
-def search_with_no_results(monkeypatch):
-    monkeypatch.setattr(
-        "media_tools.utils.get_provider",
-        lambda: MockProvider(total_samples=0)
+def provider(mocker) -> Provider:
+
+    mocker.patch("media_tools.search.Credentials")
+
+    return Provider(client_id="", client_secret="")
+
+
+@pytest.fixture
+def search_with_no_results(mocker):
+    mocker.patch(
+        "media_tools.search.Provider.search",
+        return_value=fake_search(total_samples=0)
     )
 
 
 @pytest.fixture
-def search_with_some_results(monkeypatch):
-    monkeypatch.setattr(
-        "media_tools.utils.get_provider",
-        lambda: MockProvider(total_samples=500)
+def search_with_some_results(mocker):
+    mocker.patch(
+        "media_tools.search.Provider.search",
+        return_value=fake_search(total_samples=500)
     )
 
 
 @pytest.fixture
-def search_with_too_many_results(monkeypatch):
-    monkeypatch.setattr(
-        "media_tools.utils.get_provider",
-        lambda: MockProvider(total_samples=10_000)
+def search_with_too_many_results(mocker):
+    mocker.patch(
+        "media_tools.search.Provider.search",
+        return_value=fake_search(total_samples=10_000)
     )
 
 
-def test_get_artists_with_no_results(search_with_no_results):
-    artists = search.get_artists("some-artist-query")
+def test_get_artists_with_no_results(provider: Provider, search_with_no_results):
+    artists = provider.get_artists("some-artist-query")
     assert len(artists) == 0
 
 
-def test_get_artists_with_some_results(search_with_some_results):
-    artists = search.get_artists("some-artist-query")
+def test_get_artists_with_some_results(provider: Provider, search_with_some_results):
+    artists = provider.get_artists("some-artist-query")
     assert len(artists) == 500
 
 
-def test_get_artists_with_too_many_results(search_with_too_many_results):
-    artists = search.get_artists("some-artist-query")
+def test_get_artists_with_too_many_results(provider: Provider, search_with_too_many_results):
+    artists = provider.get_artists("some-artist-query")
     assert len(artists) == 1_000
