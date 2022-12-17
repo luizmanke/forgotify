8005d978410091b2681a5ed4b4abbc442ca790b6
Luiz Felipe Manke <luizmanke@gmail.com>
2022-12-17 20:18:16 -0300

    fixup! Split run function

--- projects/batch-database-update/batch_database_update/main.py
+++ projects/batch-database-update/batch_database_update/main.py
@@ -5,13 +5,17 @@ from batch_database_update import database
 from batch_database_update import media
 
 
-def update_artists(execution_time: datetime) -> None:
+def update_artists() -> None:
+
+    execution_time = datetime.utcnow()
 
     artists = media.get_artists(list(string.ascii_uppercase))
     database.add_artists(artists, execution_time)
 
 
-def update_tracks(execution_time: datetime) -> None:
+def update_tracks() -> None:
+
+    execution_time = datetime.utcnow()
 
     artists_name = [artist.name for artist in database.get_artists()]
     tracks = media.get_tracks(artists_name)
