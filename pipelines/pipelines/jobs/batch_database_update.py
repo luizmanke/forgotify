b72112a9d33b9a5d54ed2317fc1865d4c0d9f838
Luiz Felipe Manke <luizmanke@gmail.com>
2022-12-17 17:59:19 -0300

    Refactor pipeline

--- pipelines/pipelines/jobs/batch_database_update.py
+++ pipelines/pipelines/jobs/batch_database_update.py
@@ -1,13 +1,22 @@
 from dagster import job, op
 
-from batch_database_update.main import run
+from batch_database_update import main
 
 
 @op
-def run_batch_database_update():
-    run()
+def update_artists() -> bool:
+    main.update_artists()
+    return True
+
+
+@op
+def update_tracks(previous_state: bool) -> bool:
+    main.update_tracks()
+    return True
 
 
 @job
 def batch_database_update():
-    run_batch_database_update()
+
+    update_artists_state = update_artists()
+    update_tracks(update_artists_state)
