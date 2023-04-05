dc2ce0a512ea08e1905c3dabe326c260535e46fc
Luiz Manke <luiz.manke@neoway.com.br>
2023-04-05 19:14:54 -0300

    Create scrape trigger function

--- makefiles/Lint.mk
+++ makefiles/Lint.mk
@@ -4,14 +4,15 @@ PROJECT_NAME := $(shell basename ${PROJECT_DIR})
 TEST_FOLDER := tests
 TEST_VOLUME := -v ${PROJECT_DIR}/${TEST_FOLDER}:/app/${TEST_FOLDER}
 
+DOCKER_WORKDIR := --workdir /app
 DOCKER_RUN := docker run --rm
 
 
 lint-flake8:
-	${DOCKER_RUN} ${TEST_VOLUME} ${PROJECT_NAME} flake8 --max-line-length 120
+	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint flake8 ${PROJECT_NAME} --max-line-length 120
 
 lint-mypy:
-	${DOCKER_RUN} ${TEST_VOLUME} ${PROJECT_NAME} mypy . --ignore-missing-imports
+	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint mypy ${PROJECT_NAME} . --ignore-missing-imports
 
 lint-bandit:
-	${DOCKER_RUN} ${TEST_VOLUME} ${PROJECT_NAME} bandit .
+	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint bandit ${PROJECT_NAME} .
