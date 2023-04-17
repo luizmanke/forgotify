02ce20c426afe4fe8358af2802b6c9d56adb21bf
Luiz Manke <luiz.manke@neoway.com.br>
2023-04-17 12:28:05 -0300

    Get artists

--- projects/scrape-artists/scrape_artists/main.py
+++ projects/scrape-artists/scrape_artists/main.py
@@ -1,4 +1,38 @@
-from typing import Dict
+import os
+from typing import Any, Dict
+
+from loguru import logger
+
+from media_tools.search import Provider
+
+
+def run(event: Dict, context: Any):
+
+    media_client_id = os.environ.get("MEDIA_CLIENT_ID")
+    media_client_secret = os.environ.get("MEDIA_CLIENT_SECRET")
+    query = event.get("query")
+
+    if not media_client_id:
+        raise MissingEnvVar("The 'MEDIA_CLIENT_ID' environment variable is missing")
+
+    if not media_client_secret:
+        raise MissingEnvVar("The 'MEDIA_CLIENT_SECRET' environment variable is missing")
+
+    if not query:
+        raise MissingEventKey("The 'event' argument is missing the 'query' key")
+
+    if not isinstance(query, str):
+        raise InvalidKeyType("The 'event' key 'query' must be of type 'str'")
+
+    media_provider = Provider(media_client_id, media_client_secret)
+    artists = media_provider.get_artists(query)
+    logger.info(f"Query '{query}' returned {len(artists)} artists")
+
+    # Save to bucket
+
+    return {
+        "status_code": 200
+    }
 
 
 class InvalidKeyType(Exception):
@@ -9,29 +43,5 @@ class MissingEventKey(Exception):
     pass
 
 
-class PublishError(Exception):
+class MissingEnvVar(Exception):
     pass
-
-
-def run(event, context):
-
-    _validate_input(event)
-
-    # Search artists
-
-    # Save to bucket
-
-    return {
-        "status_code": 200
-    }
-
-
-def _validate_input(event: Dict):
-
-    search = event.get("search")
-
-    if not search:
-        raise MissingEventKey("The 'event' argument is missing the 'search' key")
-
-    if not isinstance(search, str):
-        raise InvalidKeyType("The 'event' key 'search' must be of type 'str'")
--- projects/scrape-artists/tests/integration/test_main.py
+++ projects/scrape-artists/tests/integration/test_main.py
@@ -3,9 +3,17 @@ import pytest
 from scrape_artists import main
 
 
-def test_run_should_save_results_to_bucket_and_return_status_code_200():
+class FakeMediaProvider:
 
-    event = {"search": "A"}
+    def get_artists(self, *args, **kwargs):
+        return []
+
+
+def test_run_should_save_results_to_bucket_and_return_status_code_200(mocker):
+
+    mocker.patch("scrape_artists.main.Provider", return_value=FakeMediaProvider())
+
+    event = {"query": "A"}
     context = {}
 
     output = main.run(event, context)
@@ -26,8 +34,26 @@ def test_run_should_raise_if_event_does_not_contain_search_key():
 
 def test_run_should_raise_if_event_search_key_is_not_string():
 
-    event = {"search": ["A"]}
+    event = {"query": ["A"]}
     context = {}
 
     with pytest.raises(main.InvalidKeyType):
+        main.run(event, context)
+
+
+@pytest.mark.parametrize(
+    "env_var",
+    [
+        "MEDIA_CLIENT_ID",
+        "MEDIA_CLIENT_SECRET"
+    ]
+)
+def test_run_should_raise_if_event_environment_variable_is_missing(monkeypatch, env_var):
+
+    monkeypatch.delenv(env_var)
+
+    event = {"query": "A"}
+    context = {}
+
+    with pytest.raises(main.MissingEnvVar):
         main.run(event, context)
