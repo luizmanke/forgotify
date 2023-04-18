b4e93523af0243287182136a2a548c7fddb7e22c
Luiz Felipe Manke <37818079+luizmanke@users.noreply.github.com>
2023-04-18 15:01:15 -0300

    refactor: Scrape trigger (#73)
    * Fix lint
    
    * Refactor scrape trigger
    
    ---------
    
    Co-authored-by: Luiz Manke <luiz.manke@neoway.com.br>

--- makefiles/Lint.mk
+++ makefiles/Lint.mk
@@ -1,5 +1,6 @@
 PROJECT_DIR := $(shell pwd)
 PROJECT_NAME := $(shell basename ${PROJECT_DIR})
+PROJECT_FOLDER := $(subst -,_,${PROJECT_NAME})
 
 TEST_FOLDER := tests
 TEST_VOLUME := -v ${PROJECT_DIR}/${TEST_FOLDER}:/app/${TEST_FOLDER}
@@ -9,10 +10,10 @@ DOCKER_RUN := docker run --rm
 
 
 lint-flake8:
-	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint flake8 ${PROJECT_NAME} ${TEST_FOLDER} --max-line-length 120
+	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint flake8 ${PROJECT_NAME} ${PROJECT_FOLDER} ${TEST_FOLDER} --max-line-length 120
 
 lint-mypy:
-	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint mypy ${PROJECT_NAME} ${TEST_FOLDER} --ignore-missing-imports
+	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint mypy ${PROJECT_NAME} ${PROJECT_FOLDER} ${TEST_FOLDER} --ignore-missing-imports
 
 lint-bandit:
 	${DOCKER_RUN} ${DOCKER_WORKDIR} ${TEST_VOLUME} --entrypoint bandit ${PROJECT_NAME} .
--- projects/scrape-trigger/scrape_trigger/main.py
+++ projects/scrape-trigger/scrape_trigger/main.py
@@ -1,9 +1,67 @@
 import json
 import os
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import boto3
 from loguru import logger
+
+
+def run(event, context):
+
+    _check_input(event)
+
+    _publish_to_topic(
+        queries=event["queries"],
+        queue_topic_arn=os.environ["QUEUE_TOPIC_ARN"],
+        infra_endpoint_url=os.environ.get("INFRA_ENDPOINT_URL")
+    )
+
+    return {
+        "status_code": 200
+    }
+
+
+def _check_input(event: Dict):
+
+    queue_topic_arn = os.environ.get("QUEUE_TOPIC_ARN")
+    queries = event.get("queries")
+
+    if not queue_topic_arn:
+        raise MissingEnvVar("The 'QUEUE_TOPIC_ARN' environment variable is missing")
+
+    if not queries:
+        raise MissingEventKey("The 'event' argument is missing the 'queries' key")
+
+    if not isinstance(queries, List):
+        raise InvalidKeyType("The 'event' key 'queries' must be of type list")
+
+
+def _publish_to_topic(
+    queries: List[str],
+    queue_topic_arn: str,
+    infra_endpoint_url: Optional[str]
+):
+
+    queue = boto3.client(
+        service_name="sns",
+        endpoint_url=infra_endpoint_url
+    )
+
+    for query in queries:
+
+        message = json.dumps({
+            "query": query
+        })
+
+        try:
+            queue.publish(
+                TopicArn=queue_topic_arn,
+                Message=message,
+            )
+        except Exception as error:
+            raise PublishMessageError(error)
+
+        logger.info(f"Message published to topic '{queue_topic_arn}': {message}")
 
 
 class InvalidKeyType(Exception):
@@ -14,50 +72,9 @@ class MissingEventKey(Exception):
     pass
 
 
-class PublishError(Exception):
+class MissingEnvVar(Exception):
     pass
 
 
-def run(event, context):
-
-    _validate_input(event)
-
-    sns = boto3.client(
-        service_name="sns",
-        endpoint_url=os.environ.get("INFRA_ENDPOINT_URL")
-    )
-
-    search = event.get("search")
-    topic_arn = os.environ["SNS_TOPIC_ARN"]
-
-    for item in search:
-
-        message = json.dumps({
-            "search": item
-        })
-
-        try:
-            sns.publish(
-                TopicArn=topic_arn,
-                Message=message,
-            )
-        except Exception as error:
-            raise PublishError(error)
-
-        logger.info(f"Message published to topic '{topic_arn}': {message}")
-
-    return {
-        "status_code": 200,
-        "search": search
-    }
-
-
-def _validate_input(event: Dict):
-
-    search = event.get("search")
-
-    if not search:
-        raise MissingEventKey("The 'event' argument is missing the 'search' key")
-
-    if not isinstance(search, List):
-        raise InvalidKeyType("The 'event' key 'search' must be of type list")
+class PublishMessageError(Exception):
+    pass
