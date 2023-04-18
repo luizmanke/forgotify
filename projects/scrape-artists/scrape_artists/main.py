713dc0ffaeb4d349a7853f477868135b1c834c9f
Luiz Manke <luiz.manke@neoway.com.br>
2023-04-18 13:19:40 -0300

    Save to storage

--- projects/scrape-artists/scrape_artists/main.py
+++ projects/scrape-artists/scrape_artists/main.py
@@ -1,16 +1,45 @@
+import json
 import os
-from typing import Any, Dict
+from datetime import datetime
+from typing import Any, Dict, List, Optional
 
+import boto3
 from loguru import logger
 
+from media_tools.schemas import Artist
 from media_tools.search import Provider
 
 
 def run(event: Dict, context: Any):
 
+    _check_inputs(event)
+
+    artists = _get_artists(
+        query=event["query"],
+        media_client_id=os.environ["MEDIA_CLIENT_ID"],
+        media_client_secret=os.environ["MEDIA_CLIENT_SECRET"]
+    )
+
+    _save_to_storage(
+        artists,
+        bucket_name=os.environ["BUCKET_NAME"],
+        infra_endpoint_url=os.environ.get("INFRA_ENDPOINT_URL")
+    )
+
+    return {
+        "status_code": 200
+    }
+
+
+def _check_inputs(event: Dict):
+
+    bucket_name = os.environ.get("BUCKET_NAME")
     media_client_id = os.environ.get("MEDIA_CLIENT_ID")
     media_client_secret = os.environ.get("MEDIA_CLIENT_SECRET")
     query = event.get("query")
+
+    if not bucket_name:
+        raise MissingEnvVar("The 'BUCKET_NAME' environment variable is missing")
 
     if not media_client_id:
         raise MissingEnvVar("The 'MEDIA_CLIENT_ID' environment variable is missing")
@@ -24,15 +53,48 @@ def run(event: Dict, context: Any):
     if not isinstance(query, str):
         raise InvalidKeyType("The 'event' key 'query' must be of type 'str'")
 
-    media_provider = Provider(media_client_id, media_client_secret)
+
+def _get_artists(
+    query: str,
+    media_client_id: str,
+    media_client_secret: str
+) -> List[Artist]:
+
+    media_provider = Provider(
+        media_client_id,
+        media_client_secret
+    )
+
     artists = media_provider.get_artists(query)
     logger.info(f"Query '{query}' returned {len(artists)} artists")
 
-    # Save to bucket
+    return artists
 
-    return {
-        "status_code": 200
-    }
+
+def _save_to_storage(
+    artists: List[Artist],
+    bucket_name: str,
+    infra_endpoint_url: Optional[str]
+):
+    current_time = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+
+    storage = boto3.client(
+        service_name="s3",
+        endpoint_url=infra_endpoint_url
+    )
+
+    for artist in artists:
+
+        try:
+            storage.put_object(
+                Body=json.dumps(artist.dict()),
+                Bucket=bucket_name,
+                Key=f"{artist.id}_{current_time}.json"
+            )
+        except Exception as error:
+            raise SaveToStorageError(error)
+
+    logger.info(f"{len(artists)} artists saved to storage")
 
 
 class InvalidKeyType(Exception):
@@ -44,4 +106,8 @@ class MissingEventKey(Exception):
 
 
 class MissingEnvVar(Exception):
+    pass
+
+
+class SaveToStorageError(Exception):
     pass
--- projects/scrape-artists/tests/integration/test_main.py
+++ projects/scrape-artists/tests/integration/test_main.py
@@ -1,26 +1,77 @@
+import json
+import os
+from typing import Dict, List
+
+import boto3
 import pytest
+from freezegun import freeze_time
 
 from scrape_artists import main
 
 
 class FakeMediaProvider:
 
-    def get_artists(self, *args, **kwargs):
-        return []
+    def get_artists(self, *args, **kwargs) -> List[main.Artist]:
+        return [
+            main.Artist(
+                id="0",
+                name="name",
+                n_followers=0,
+                genres=["genre"],
+                popularity=0.0
+            )
+        ]
 
 
-def test_run_should_save_results_to_bucket_and_return_status_code_200(mocker):
+class FakeStorage:
 
-    mocker.patch("scrape_artists.main.Provider", return_value=FakeMediaProvider())
+    def __init__(self):
+        self._storage = boto3.client(
+            service_name="s3",
+            endpoint_url=os.environ["INFRA_ENDPOINT_URL"]
+        )
+
+    def get(self, file: str) -> Dict:
+        item = self._storage.get_object(
+            Bucket=os.environ["BUCKET_NAME"],
+            Key=file
+        )
+        content = item["Body"].read()
+        return json.loads(content)
+
+
+@pytest.fixture
+def mock_media_provider(mocker):
+    mocker.patch(
+        "scrape_artists.main.Provider",
+        return_value=FakeMediaProvider()
+    )
+
+
+@pytest.fixture
+def storage():
+    return FakeStorage()
+
+
+@freeze_time("2023-01-01")
+def test_run_should_save_results_to_bucket_and_return_status_code_200(
+    mock_media_provider,
+    storage
+):
 
     event = {"query": "A"}
     context = {}
 
     output = main.run(event, context)
 
-    # Assert bucket save
-
     assert output == {"status_code": 200}
+    assert storage.get(file="0_20230101_000000.json") == {
+        "id": "0",
+        "name": "name",
+        "n_followers": 0,
+        "genres": ["genre"],
+        "popularity": 0.0
+    }
 
 
 def test_run_should_raise_if_event_does_not_contain_search_key():
@@ -44,11 +95,12 @@ def test_run_should_raise_if_event_search_key_is_not_string():
 @pytest.mark.parametrize(
     "env_var",
     [
+        "BUCKET_NAME",
         "MEDIA_CLIENT_ID",
         "MEDIA_CLIENT_SECRET"
     ]
 )
-def test_run_should_raise_if_event_environment_variable_is_missing(monkeypatch, env_var):
+def test_run_should_raise_if_environment_variable_is_missing(monkeypatch, env_var):
 
     monkeypatch.delenv(env_var)
 
@@ -56,4 +108,18 @@ def test_run_should_raise_if_event_environment_variable_is_missing(monkeypatch, 
     context = {}
 
     with pytest.raises(main.MissingEnvVar):
+        main.run(event, context)
+
+
+def test_run_should_raise_if_save_to_storage_fails(
+    mock_media_provider,
+    monkeypatch
+):
+
+    monkeypatch.setenv("BUCKET_NAME", "wrong-bucket-name")
+
+    event = {"query": "A"}
+    context = {}
+
+    with pytest.raises(main.SaveToStorageError):
         main.run(event, context)
